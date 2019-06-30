d74e564fff8090b05ffdfd8cb7401271c297a2d1
alexthedar <alex.castaneda@gmail.com>
2019-06-30 08:37:39 -0700

    Layout base

--- src/hoc/Layout.jsx
+++ src/hoc/Layout.jsx
@@ -1,7 +1,72 @@
 import React from "react";
 import { connect } from "react-redux";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import { Container } from "@material-ui/core";
 // import { Navbar, Container, Nav, Spinner, Alert } from "react-bootstrap";
 // import SearchForm from "../components/Search";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
+}));
+
+
 
 const generateAppContents = props => {
   const { error, isFetching } = props;
@@ -20,12 +85,44 @@ const generateAppContents = props => {
 };
 
 export const Layout = props => {
+  const classes = useStyles();
   return (
-    <div>
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative" variant="dense">
+        <Toolbar>
+          {/* <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Open drawer"
+          >
+            <MenuIcon />
+          </IconButton> */}
+          <Typography className={classes.title} variant="h6" noWrap>
+            Movies
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'Search' }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
       <main style={{ paddingTop: "1.5rem", marginBottom: "1.5rem" }}>
-        {generateAppContents(props)}
+        <Container maxWidth="lg">
+          {generateAppContents(props)}
+        </Container>
       </main>
-    </div>
+    </React.Fragment>
   );
 };
 
@@ -38,3 +135,6 @@ export default connect(
   mapStateToProps,
   null
 )(Layout);
+
+
+
