135f915493a9976d16955d740d2f3fa2dafd9582
alexthedar <alex.castaneda@gmail.com>
2019-06-30 11:46:13 -0700

    axios

--- src/api/iex-get.js
+++ src/api/iex-get.js
@@ -1,14 +1,51 @@
-const baseURL = "https://api.iextrading.com/1.0";
+import axios from "axios";
 
+// https://api.themoviedb.org/3/trending/all/day?api_key=057dfa32a18eed0f2dc23dc2e80ed8a0
+
+const baseURL = "https://api.iextrading.com/1.0";
+// const baseURL = "https://api.themoviedb.org/3/search/movie?page=1&include_adult=false&language=en-US&api_key='057dfa32a18eed0f2dc23dc2e80ed8a0'";
+const movieDbBaseURL = "https://api.themoviedb.org/3";
+const apiKey = "057dfa32a18eed0f2dc23dc2e80ed8a0";
+// const baseURL = `https://api.themoviedb.org/3/&api_key=${apiKey}`;
+//
+// query
+//
+
+// export const search = (searchString) => {
+// export const refDataSymbols = (searchString) => {
+//   const url = `${baseURL}&query=raider`;
+//   // const url = `${baseURL}/&query=${searchString}`;
+//   console.log(url);
+
+//   return fetch(url)
+//   .then(res => Promise.resolve(res.json()))
+//     .catch(error => Promise.reject(error));
+// };
 //
 // reference
 //
 
-export const refDataSymbols = () => {
-  const url = `${baseURL}/ref-data/symbols`;
-  return fetch(url)
-  .then(res => Promise.resolve(res.json()))
-    .catch(error => Promise.reject(error));
+// export const refDataSymbols = () => {
+//   const url = `${baseURL}/ref-data/symbols`;
+//   return fetch(url)
+//   .then(res => Promise.resolve(res.json()))
+//     .catch(error => Promise.reject(error));
+// };
+export const get = () => {
+  return axios
+    .get(`${movieDbBaseURL}${}?api_key=${apiKey}`)
+    .then(function(response) {
+      // handle success
+      console.log("HERE", response);
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function() {
+      // always executed
+      console.log("FINISHED");
+    });
 };
 
 //
@@ -17,19 +54,17 @@ export const refDataSymbols = () => {
 
 export const quotesForSymbol = symbol => {
   const url = `${baseURL}/stock/${symbol}/quote`;
-  return (
-    fetch(url)
-      .then(res => Promise.resolve(res.json()))
-      .catch(error => Promise.reject(error))
-  );
+  return fetch(url)
+    .then(res => Promise.resolve(res.json()))
+    .catch(error => Promise.reject(error));
 };
 
 export const logoForSymbol = symbol => {
   const url = `${baseURL}/stock/${symbol}/logo`;
 
   return fetch(url)
-  .then(res => Promise.resolve(res.json()))
-  .catch(error => Promise.reject(error));
+    .then(res => Promise.resolve(res.json()))
+    .catch(error => Promise.reject(error));
 };
 
 export const recentNewsForSymbol = symbol => {
